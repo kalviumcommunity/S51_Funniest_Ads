a451cf31e7a1830bb70569b418b0135a265db875
AdityaKannur <adityakannur28@gmail.com>
2024-02-24 10:46:17 +0530

    created LandingPage

---
+++ client/react-app/src/component/LandingPage.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+import './LandingPage.css'
+
+export const LandingPage = () => {
+    return <div>
+        <h1>Hello there!</h1>
+        <div className="greeting">Welcome the my project page</div>
+    </div>
+}
