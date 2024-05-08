8167cd10e63585b8347e103c22014c0d79087c0f
AdityaKannur <adityakannur28@gmail.com>
2024-05-08 17:14:51 +0530

    validated

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/createUser', async (req, res) => {
 
 
         // If validation succeeds, proceed with creating the user
-        const newUser = new User(value); // Use validated data
+        const newUser = new User(value); 
         await newUser.save();
         res.status(201).json(newUser);
     } catch (err) {
